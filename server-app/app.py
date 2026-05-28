d838295adfb0f0fa582e73b0f59bf82bb3d65dd0
dev-ops-bhojaiya <253447196+shailesh-Ops@users.noreply.github.com>
2026-05-28 05:31:07 +0100

    fix bug on for browser isusse

--- server-app/app.py
+++ server-app/app.py
@@ -7,11 +7,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return {"message": source , "result": "DEvOps Engineer to DevSecOps", "version": source1, "port": 9000}
+    return {"message": source ,
+            "result": "DEvOps Engineer to DevSecOps", 
+            "version": source1, "port": 9000         
+        }
 
 @app.route("/health")
 def index_second():
-    return {"status": f"version v-11 latest", "data1": source1}
+    return {"status": "version v-11 latest",
+            "data1": source1
+        }
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9000, debug=True)
