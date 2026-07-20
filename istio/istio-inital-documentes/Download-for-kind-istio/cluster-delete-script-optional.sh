422cbf2566c867ddc7bba410dbf781db30c35733
Dev-shailesh <253447196+shailesh-Ops@users.noreply.github.com>
2026-07-20 10:37:22 +0100

    info-graphical doc's

---
+++ istio/istio-inital-documentes/Download-for-kind-istio/cluster-delete-script-optional.sh
@@ -0,0 +1,6 @@
+echo 'When you are done experimenting and you want to delete the existing cluster, use the following command:'
+
+
+ kind delete cluster --name istio-testing
+
+# Deleting cluster "istio-testing" ...
