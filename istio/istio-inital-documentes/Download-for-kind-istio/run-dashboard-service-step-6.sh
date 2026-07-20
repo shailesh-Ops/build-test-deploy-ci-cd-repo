7b285b591aa8653b476b7a0315582b51805de3b2
Dev-shailesh <253447196+shailesh-Ops@users.noreply.github.com>
2026-07-20 11:13:15 +0100

    file for run dashboard

---
+++ istio/istio-inital-documentes/Download-for-kind-istio/run-dashboard-service-step-6.sh
@@ -0,0 +1,7 @@
+
+echo "run this command then second one is browser base insert point "
+echo   " first command is ++++++++  kubectl proxy port=8001 --address=0.0.0.0 --accept-hosts='.*'"
+
+
+echo "browser base ======== http://127.0.0.1:8001127.0.0.1:8001api/v1/proxy/namespaces/kube-system/services/kubernetes-dashboard:/proxy" 
+
